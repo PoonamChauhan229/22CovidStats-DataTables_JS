435970e7566bfe7ddfd4452d79a0ab1bacbde0a3
Poonam Chauhan <68181154+PoonamChauhan229@users.noreply.github.com>
2024-02-05 17:05:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,16 @@
-// async function getData(){
-//     tbody=document.getElementById('tbody')
-//     let data=await fetch('https://api.covid19api.com/summary')
-//     let res=await data.json()
-//     console.log(res.Countries)
-//     res.Countries.map((element)=>{
-//         tbody.innerHTML+=`
-//         <tr>
-//         <td>${element.Country}</td>
-//         <td>${element.TotalConfirmed}</td>
-//         <td>${element.TotalConfirmed-element.TotalRecovered}</td>
-//         <td>${element.TotalRecovered}</td>
-//         <td>${element.TotalDeaths}</td>
-
-//     </tr>
-        
-//         `
-//     })
-
-// }
-
-// getData()
-
-// $(document).ready( function () {
-//     $('#covidTable').DataTable();
-// } );
-
-
 $(document).ready( async function (){
     tbody=document.getElementById('tbody')
-    let data=await fetch('https://api.covid19api.com/summary')
+    let data=await fetch("./data.json")
     let res=await data.json()
-    console.log(res.Countries)
+    console.log(res.data)
     res.Countries.map((element)=>{
         tbody.innerHTML+=`
         <tr>
-        <td>${element.Country}</td>
-        <td>${element.TotalConfirmed}</td>
-        <td>${element.TotalConfirmed-element.TotalRecovered}</td>
-        <td>${element.TotalRecovered}</td>
-        <td>${element.TotalDeaths}</td>
+        <td>${element.name}</td>
+        <td>${element.infected}</td>
+        <td>${element.infected-element.recovered}</td>
+        <td>${element.recovered}</td>
+        <td>${element.dead}</td>
 
     </tr>
         
